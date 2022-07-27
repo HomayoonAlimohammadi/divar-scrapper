0e9122316d36e4a616be01a3669890b7f51e12bc
homayoonalimohammadi <homayoonalimohammadi@gmail.com>
2022-07-27 08:31:56 +0430

    Fix data storage in json and txt

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
 from __future__ import annotations
 import pathlib
-import requests
-from bs4 import BeautifulSoup
 import json
+from bs4 import BeautifulSoup
+import requests
 
 
 class DivarParser:
@@ -15,26 +15,27 @@ class DivarParser:
         self.base_url = "https://www.divar.ir"
         self.region = region
         self.paths = paths or self.get_paths()
+        self._n_results = n_results
 
     def get_paths(self) -> list[str]:
         res = requests.get(self.base_url + self.region)
         parsed = BeautifulSoup(res.content, "html.parser")
 
-        paths = []
+        paths: list[str] = []
         filter_box = parsed.find("div", class_="filter-box")
         sections = filter_box.find_all("a", "kt-accordion-item__header")  # type: ignore
-        for section in sections:
-            paths.append(section["href"])
+        for section in sections:  # type: ignore
+            paths.append(section["href"])  # type: ignore
         return paths
 
     def get_section_titles(self) -> dict[str, list[str]]:
-        data = {}
+        data: dict[str, list[str]] = {}
         for path in self.paths:
+            titles: list[str] = []
             res = requests.get(f"{self.base_url}{path}")
             parsed = BeautifulSoup(res.content, "html.parser")
             results = parsed.find_all("div", class_="post-card-item")
-            titles = []
-            for i, item in enumerate(results[:20]):
+            for _, item in enumerate(results[: self._n_results]):
                 header = item.find("h2", class_="kt-post-card__title")
                 titles.append(header.text.strip())
             data[path] = titles
@@ -47,11 +48,12 @@ class DivarParser:
         if data is None:
             data = self.get_section_titles()
         with open(file_path, "w") as f:
-            json.dump(data, f)
+            json.dump(data, f, ensure_ascii=False)
 
 
 divar_parser = DivarParser()
 data = divar_parser.get_section_titles()
 with open("data.txt", "w") as f:
     f.write(str(data))
-divar_parser.dump_section_titles("divar-data.json", data)
+divar_parser.dump_section_titles("./divar-data.json", data)
+# TODO: output redirect
